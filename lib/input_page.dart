e6e5e25ceef592945144f46ff851d48125681f38
Rashid Razak <bib.nexus@gmail.com>
2019-08-14 03:07:22 +0800

    Extract repetitive code into a new reusable widget.

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -19,13 +19,7 @@ class _InputPageState extends State<InputPage> {
               child: Row(
                 children: <Widget>[
                   Expanded(
-                    child: Container(
-                      margin: EdgeInsets.all(15.0),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10.0),
-                        color: Color(0xFF1D1E33),
-                      ),
-                    ),
+                    child: new ReusableCard(),
                   ),
                   Expanded(
                     child: Container(
@@ -74,6 +68,23 @@ class _InputPageState extends State<InputPage> {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  const ReusableCard({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        color: Color(0xFF1D1E33),
       ),
     );
   }
