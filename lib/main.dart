f1178c5fb4d7aeca619dea4c57d411bdacc0ff38
Rashid Razak <bib.nexus@gmail.com>
2019-08-14 02:12:59 +0800

    Add basic starter code.

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,31 @@ void main() => runApp(BMICalculator());
 class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: InputPage(),
+    );
   }
 }
 
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI Calculator'),
+      ),
+      body: Center(
+        child: Text('Body Text'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
