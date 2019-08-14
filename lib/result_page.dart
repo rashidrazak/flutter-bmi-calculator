fd497a96aad7e336eb26f05a48e16f12949d7330
Rashid Razak <bib.nexus@gmail.com>
2019-08-15 07:21:54 +0800

    Add new page - ResultPage.

---
+++ lib/result_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ResultPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Text('Result Page'),
+    );
+  }
+}
